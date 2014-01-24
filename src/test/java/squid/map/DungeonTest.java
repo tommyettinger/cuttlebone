b480523ddc21a464bc07b9551a3c57ffe892fe74
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-01-23 22:43:54 -0800

    Herringbone tiles are loaded from file.

---
+++ src/test/java/squid/map/DungeonTest.java
@@ -0,0 +1,12 @@
+package squid.map;
+import squid.squidgrid.map.HerringboneDungeon;
+public class DungeonTest
+{
+    
+    public static void main(String[] args)
+    {
+        HerringboneDungeon hbd = new HerringboneDungeon(80,20);
+        System.out.println(hbd);
+    }
+    
+}
