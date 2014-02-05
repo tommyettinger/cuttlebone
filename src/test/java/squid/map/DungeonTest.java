ddacf05aa0a40aa21117abe3d3ba16c6820931ec
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-02-05 04:31:23 -0800

    RunningBondDungeon uses a concentric pattern.

--- src/test/java/squid/map/DungeonTest.java
+++ src/test/java/squid/map/DungeonTest.java
@@ -5,16 +5,28 @@ import java.io.FileNotFoundException;
 
 import squid.squidgrid.map.BrickDungeon;
 import squid.squidgrid.map.HerringboneDungeon;
+import squid.squidgrid.map.RunningBondDungeon;
+import squid.squidmath.RNG;
 public class DungeonTest
 {
     
     public static void main(String[] args)
     {
-        BrickDungeon hbd;
+        BrickDungeon bd;
+        HerringboneDungeon hbd;
+        RunningBondDungeon rbd;
+        byte[] seed = {15, 17, 1, 57, -100, -77, 78, 86, 66, -120, 42, 93, 19, 127, -127, -2};
         try
         {
-            hbd = new BrickDungeon(80, 30, new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
+            bd = new BrickDungeon(80, 30, new RNG(seed), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
+            hbd = new HerringboneDungeon(80, 30, new RNG(seed), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
+            rbd = new RunningBondDungeon(80, 30, new RNG(seed), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
             System.out.println(hbd);
+            System.out.println();
+            System.out.println(bd);
+            System.out.println();
+            System.out.println(rbd);
+            System.out.println();
         } catch (FileNotFoundException e)
         {
             // TODO Auto-generated catch block
