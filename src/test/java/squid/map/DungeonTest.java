0b6be05e434a75e10da5c04bf1570f8b084f84c2
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-02-05 23:01:07 -0800

    Updated test to make sense for profiling maybe

--- src/test/java/squid/map/DungeonTest.java
+++ src/test/java/squid/map/DungeonTest.java
@@ -1,4 +1,5 @@
 package squid.map;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,6 +8,7 @@ import squid.squidgrid.map.BrickDungeon;
 import squid.squidgrid.map.HerringboneDungeon;
 import squid.squidgrid.map.RunningBondDungeon;
 import squid.squidmath.RNG;
+
 public class DungeonTest
 {
     
@@ -15,20 +17,44 @@ public class DungeonTest
         BrickDungeon bd;
         HerringboneDungeon hbd;
         RunningBondDungeon rbd;
-        byte[] seed = {15, 17, 1, 57, -100, -77, 78, 86, 66, -120, 42, 93, 19, 127, -127, -2};
+        byte[] seed =
+            {
+                    15, 17, 1, 57, -100, -77, 78, 86, 66, -120, 42, 93, 19,
+                    127, -127, -2
+            };
         try
         {
-            bd = new BrickDungeon(80, 24, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-            hbd = new HerringboneDungeon(39, 40, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-            rbd = new RunningBondDungeon(133, 23, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-
-            bd = new BrickDungeon(80, 24, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-            hbd = new HerringboneDungeon(39, 40, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-            rbd = new RunningBondDungeon(133, 23, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-
-            bd = new BrickDungeon(80, 24, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-            hbd = new HerringboneDungeon(39, 40, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
-            rbd = new RunningBondDungeon(133, 23, new RNG(), new FileInputStream(new File("resources/centeredHoriz.txt")), new FileInputStream(new File("resources/centeredVert.txt")));
+            bd = new BrickDungeon(80, 24, new RNG(), new FileInputStream(
+                    new File("resources/centeredHoriz.txt")),
+                    new FileInputStream(new File("resources/centeredVert.txt")));
+            hbd = new HerringboneDungeon(
+                    80,
+                    24,
+                    new RNG(),
+                    new FileInputStream(new File("resources/centeredHoriz.txt")),
+                    new FileInputStream(new File("resources/centeredVert.txt")));
+            rbd = new RunningBondDungeon(
+                    80,
+                    24,
+                    new RNG(),
+                    new FileInputStream(new File("resources/centeredHoriz.txt")),
+                    new FileInputStream(new File("resources/centeredVert.txt")));
+            for (int i = 0; i < 100; i++)
+            {
+                
+                bd = new BrickDungeon(80, 24, new RNG(), null, null);
+                hbd = new HerringboneDungeon(80, 24, new RNG(), null, null);
+                rbd = new RunningBondDungeon(80, 24, new RNG(), null, null);
+                System.out.print(bd.get1DShown()[0] + hbd.get1DShown()[0] + rbd.get1DShown()[0]);
+                bd = new BrickDungeon(39, 40, new RNG(), null, null);
+                hbd = new HerringboneDungeon(39, 40, new RNG(), null, null);
+                rbd = new RunningBondDungeon(39, 40, new RNG(), null, null);
+                System.out.print(bd.get1DShown()[0] + hbd.get1DShown()[0] + rbd.get1DShown()[0]);
+                bd = new BrickDungeon(133, 23, new RNG(), null, null);
+                hbd = new HerringboneDungeon(133, 23, new RNG(), null, null);
+                rbd = new RunningBondDungeon(133, 23, new RNG(), null, null);
+                System.out.print(bd.get1DShown()[0] + hbd.get1DShown()[0] + rbd.get1DShown()[0]);
+            }
             
             System.out.println(bd);
             System.out.println();
@@ -36,6 +62,7 @@ public class DungeonTest
             System.out.println();
             System.out.println(rbd);
             System.out.println();
+            
         } catch (FileNotFoundException e)
         {
             // TODO Auto-generated catch block
